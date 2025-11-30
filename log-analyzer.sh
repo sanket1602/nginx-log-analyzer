dd39fee1e41bf010b56b5ca333268b4c5e0babd2
Mr.Robot <99020575+sanket1602@users.noreply.github.com>
2025-11-30 10:03:06 +0530

    change log file name in script

--- log-analyzer.sh
+++ log-analyzer.sh
@@ -3,13 +3,13 @@ COUNTS=5
 
 
 echo -e "\nTop ${COUNTS} IP addresses with the most requests:"
-awk '{print $1}' logfile.txt | sort | uniq -c | sort -nr |awk '{print $2 " - " $1 " requests"}' | head -n ${COUNTS}
+awk '{print $1}' nginx-log.txt | sort | uniq -c | sort -nr |awk '{print $2 " - " $1 " requests"}' | head -n ${COUNTS}
 
 echo -e "\nTop ${COUNTS} most requested paths:"
-awk '{print $7}' logfile.txt | sort | uniq -c | sort -nr |awk '{print $2 " - " $1 " requests"}' | head -n ${COUNTS}
+awk '{print $7}' nginx-log.txt | sort | uniq -c | sort -nr |awk '{print $2 " - " $1 " requests"}' | head -n ${COUNTS}
 
 echo -e "\nTop ${COUNTS} response status codes:"
-grep -oE ' [1-5][0-9]{2} ' logfile.txt | sort | uniq -c | sort -rn | awk '{print $2 " - " $1 " requests"}' |  head -n $COUNTS 
+grep -oE ' [1-5][0-9]{2} ' nginx-log.txt | sort | uniq -c | sort -rn | awk '{print $2 " - " $1 " requests"}' |  head -n $COUNTS 
 
 echo -e "\nTop ${COUNTS} user agents:"
-awk -F'"' '{print $6}' logfile.txt | sort | uniq -c | sort -nr | awk '{for(i=2;i<=NF;i++) printf "%s ", $i; print "-",$1,"requests"}' | head -n $COUNTS
+awk -F'"' '{print $6}' nginx-log.txt | sort | uniq -c | sort -nr | awk '{for(i=2;i<=NF;i++) printf "%s ", $i; print "-",$1,"requests"}' | head -n $COUNTS
